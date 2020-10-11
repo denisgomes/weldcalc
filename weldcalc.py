f59a8099011ff798af374cdce52f0a1762a2c9b0
Denis Gomes <dgomes@hotmail.com>
2020-10-11 19:29:42 -0400

    Bug fix of iyy and izz

--- weldcalc.py
+++ weldcalc.py
@@ -20,6 +20,7 @@ from numpy import linalg as la
 
 
 class WeldLine:
+    """Single line weld"""
 
     def __init__(self, from_point, to_point, size, weld_type="fillet"):
         self._from_point = np.array([*from_point, 1])
@@ -88,7 +89,7 @@ class WeldLine:
 
         See references [1] and [2].
         """
-        Ix = (1/12) * self.length()**3 * self.throat()
+        Ix = (1/12) * self.length()**3
         Iy = 0
         Iz = Ix + Iy
 
@@ -101,7 +102,8 @@ class WeldLine:
         R = np.array([*self.center(), 0])
         I = self.rmat() @ imat @ np.transpose(self.rmat())  # about cg
 
-        return I + self.length()*(np.dot(R, R)*np.eye(3) - np.outer(R, R))
+        return (I + self.length()*(np.dot(R, R)*np.eye(3) -
+                np.outer(R, R))) * self.throat()
 
     def ixx(self):
         """Inertia of weld line about the x axis with respect to coordinate
@@ -118,62 +120,81 @@ class WeldLine:
         return self.inertia()[2, 2]
 
 
-class WeldProfile:
-    pass
-
-
 class WeldGroup:
-    """A planar weld group consisting of a similar weld size"""
+    """A planar weld group consisting of serveral weld lines with similar or
+    varying weld sizes.
+    """
 
-    def __init__(self, name, size, weld_type="fillet"):
-        self.name = name
-        self.size = size
-        self.weld_type = weld_type
+    def __init__(self):
         self.weld_lines = []
 
-    def add_weld_line(self, x1, y1, z1, x2, y2, z2):
-        self.weld_lines.append((x1, y1, z1, x2, y2, z2))
+    def add_weld_line(self, from_point, to_point, size, weld_type):
+        wl = WeldLine(from_point, to_point, size, weld_type)
+        self.weld_lines.append(wl)
 
     def center(self):
         # average of components in each direction
-        xsum, ysum, zsum = 0, 0, 0
+        xsum, ysum = 0, 0
         for weld_line in self.weld_lines:
-            x1, y1, z1, x2, y2, z2 = weld_line
-            xsum += (x1 + x2)
-            ysum += (y1 + y2)
-            zsum += (z1 + z2)
+            cx, cy = weld_line.center()
+            xsum += cx
+            ysum += cy
 
         ncoords = 2 * len(self.weld_lines)
 
-        return xsum/ncoords, ysum/ncoords, zsum/ncoords
+        return xsum/ncoords, ysum/ncoords
 
     def length(self):
         """Length of weld group"""
-        dist = 0
+        group_length = 0
         for weld_line in self.weld_lines:
-            x1, y1, z1, x2, y2, z2 = weld_line
-            dist += np.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
+            group_length += weld_line.length()
 
-        return dist
-
-    def throat(self):
-        """Throat size of weld"""
-        if self.weld_type == "fillet":
-            return 1/np.sqrt(2) * self.size
+        return group_length
 
     def area(self):
-        """Weld group area"""
-        return self.throat() * self.length()
+        """Area of weld group"""
+        group_area = 0
+        for weld_line in self.weld_lines:
+            group_area += weld_line.area()
 
-    def inertia(self, offset=(0, 0, 0)):
-        """Unit length of weld group"""
-        return (1/12)*1*self.length()**3 * self.throat()
+        return group_area
+
+    def ixx(self):
+        group_ixx = 0
+        for weld_line in self.weld_lines:
+            group_ixx += weld_line.ixx()
+
+        return group_ixx
+
+    def iyy(self):
+        group_iyy = 0
+        for weld_line in self.weld_lines:
+            group_iyy += weld_line.iyy()
+
+        return group_iyy
+
+    def izz(self):
+        group_izz = 0
+        for weld_line in self.weld_lines:
+            group_izz += weld_line.izz()
+
+        return group_izz
 
 
 if __name__ == "__main__":
-    wl = WeldLine((0, -5, 0), (0, 5, 0), 0.25, "fillet")
+    wg = WeldGroup()
+    wg.add_weld_line((-5, -5, 0), (-5, 5, 0), 0.25, "fillet")
+    wg.add_weld_line((5, -5, 0), (5, 5, 0), 0.25, "fillet")
 
-    print(wl.center())
-    print(wl.length())
-    print(wl.area())
-    print(wl.ixx())
+    print(wg.center())
+    print(wg.length())
+    print(wg.area())
+    print(wg.ixx())
+
+    # wl = WeldLine((-5, -5, 0), (-5, 5, 0), 0.25, "fillet")
+    # wl = WeldLine((0, -5, 0), (0, 5, 0), 0.25, "fillet")
+    # print(wl.center())
+    # print(wl.length())
+    # print(wl.area())
+    # print(wl.ixx())
